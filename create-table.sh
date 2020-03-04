69921a9cbc7da4fd1933692312bb9aa42f6ba98d
Rune Nielsen <runeanielsen@live.dk>
2020-03-04 08:53:59 +0100

    Fixed shell issues

--- create-table.sh
+++ create-table.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-psql -h localhost -p 5432 -U postgres
+psql -h localhost -p 5432 -U postgres;
 CREATE DATABASE inventory;
 \c inventory;
 CREATE TABLE dumb_table(id SERIAL PRIMARY KEY, name VARCHAR);
